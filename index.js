28d3f26057b0db655eb232d71d0c6507210c8105
Jono Muller <jonomuller@gmail.com>
2017-02-24 18:56:00 +0000

    Add listen function to index.js to prevent app crash

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', function (req, res) {
   res.send('<html><body><h1>Hello World</h1></body></html>');
 });
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
