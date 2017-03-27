d848982b71ebbf971ef6e797cdcddca5c2bc33ea
Jono Muller <jonomuller@gmail.com>
2017-03-27 23:37:37 +0100

    Implement local user login with username+password using passport-local

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,8 @@
 const passport = require('passport'),
       passportJWT = require('passport-jwt'),
-      ExtractJWT = passportJWT.ExtractJwt,
+      LocalStrategy = require('passport-local').Strategy,
       JwtStrategy = passportJWT.Strategy,
+      ExtractJWT = passportJWT.ExtractJwt,
       config = require('./main'),
       User = require('../models/user');
 
@@ -10,6 +11,23 @@ const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeader(),
 }
 
+// Used to log user in via username and password
+passport.use(new LocalStrategy(function (username, password, done) {
+  User.findOne({username: username}, function(error, user) {
+    if (error) return done(error);
+    if (!user) return done(null, false, );
+
+    user.comparePassword(password, function(error, success) {
+      if (error) return done(error);
+      if (!success) return done(null, false);
+
+      done(null, user);
+    });
+  });
+}));
+
+// Used to verify if user is authenticated without them having to log in again
+// with username and password
 passport.use(new JwtStrategy(options, function(jwt_payload, done) {
   User.findById(jwt_payload._id, function(error, user) {
     if (error) return done(error, false);
