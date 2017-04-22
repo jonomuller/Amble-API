5f42c7aca121b4053da8968d1b4f3a455ad63d42
Jono Muller <jonomuller@gmail.com>
2017-04-22 03:08:33 +0100

    Successfully store a walk in the database Use GEOJson Mongoose library to store array of coordinates representing a walk Return error if no JWT provided or if fields are missing

--- controllers/walks.js
+++ controllers/walks.js
@@ -1,7 +1,38 @@
 const Walk = require('../models/walk');
 
 module.exports.create = function(req, res, next) {
-  res.json({
-    test: 'test'
+  var name = req.body.name;
+  var owner = req.body.owner;
+  var coordinates = JSON.parse(req.body.coordinates)
+
+  var required = {
+    "name": name,
+    "owner": owner,
+    "coordinates": coordinates
+  };
+
+  for (let key in required) {
+    var value = required[key];
+    if (!value) return res.status(400).json({
+                         success: false,
+                         error: `Please enter the ${key}.`
+                       })
+  }
+
+  var walk = new Walk({
+    name,
+    owner,
+    geometry: {
+      type: 'MultiPoint',
+      coordinates: coordinates
+    }
+  });
+
+  walk.save(function(error) {
+    if (error) return next(error);
+    res.status(200).json({
+      success: true,
+      walk: walk
     });
+  });
 };
