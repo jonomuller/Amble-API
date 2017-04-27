908e58b2134690eeb073aa9c404a049c58f4f4a7
Jono Muller <jonomuller@gmail.com>
2017-04-28 00:19:43 +0100

    Update error handler function to support errors with only one message

--- controllers/helper.js
+++ controllers/helper.js
@@ -1,11 +1,18 @@
 module.exports.mongooseValidationError = function(error, res) {
-  for (let key in error.errors) {
-    let message = error.errors[key].message;
-    if (message != undefined) {
-      return res.status(400).json({
-               success: false,
-               error: message
-             });
+  var message = 'Validation error';
+
+  if (error.message != undefined) {
+    message = error.message;
+  } else {
+    for (let key in error.errors) {
+      if (error.errors[key].message != undefined) {
+        message = error.errors[key].message;
+      }
     }
   }
+
+  return res.status(400).json({
+    success: false,
+    error: message
+  });
 };
