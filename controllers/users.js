7d5dac3307372b279e372847633227a5dfe361c2
Jono Muller <jonomuller@gmail.com>
2017-04-29 03:24:23 +0100

    Only return basic details from getWalks instead of entire walk object Only walk ID and name are returned right now

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,22 @@ const Walk = require('../models/walk'),
 
 module.exports.getWalks = function(req, res, next) {
   var id = req.params.userID;
-  
+
   Walk.find({owner: id}, function(error, walks) {
     if (error) return helper.mongooseValidationError(error, res);
 
+    var walkDetails = [];
+    
+    walks.forEach(function(walk) {
+      walkDetails.push({
+        id: walk._id,
+        name: walk.name
+      });
+    });
+
     return res.status(200).json({
       success: true,
-      walks: walks
+      walks: walkDetails
     });
   });
 };
