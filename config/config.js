c9ecf3afc7e55487b40d41ab61be06d880001f7c
Jono Muller <jonomuller@gmail.com>
2017-05-11 17:26:25 +0100

    Change .env file to use JSON to work with APNs private key

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const env = require('env2')('.env');
+const env = require('env2')('env.json');
 
 module.exports = {
   port: process.env.PORT || 3000,
@@ -8,6 +8,7 @@ module.exports = {
   awsAccessKeyID: process.env.AWS_ACCESS_KEY_ID,
   awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   jwtSecret: process.env.JWT_SECRET,
+  apnsPrivateKey: process.env.APNS_PRIVATE_KEY,
   jwtSession: {
     session: false
   }
