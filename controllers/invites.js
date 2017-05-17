5b07e45912e3ce568a9b88ca25c2c3d4873b6fb1
Jono Muller <jonomuller@gmail.com>
2017-05-17 17:17:23 +0100

    Create endpoint to start walk if user is the invite owner GET /invites/:inviteID/start_walk For now the endpoint just deletes the given invite, but in future will be used to show live walk information to invite recipients

--- controllers/invites.js
+++ controllers/invites.js
@@ -4,12 +4,9 @@ const Invite = require('../models/invite'),
 module.exports.acceptInvite = function(req, res, next) {
   Invite.findById(req.params.inviteID, function(error, invite) {
     if (error) return helper.mongooseValidationError(error, res);
+    if (!invite) return inviteDoesNotExist(res);
 
-    if (!invite) return res.status(404).json({
-      success: false,
-      error: 'Invite does not exist.'
-    })
-
+    if (!checkInviteAuth(req.user._id, res, invite.to)) return inviteAuthError(res);
     invite.accepted = true;
 
     invite.save(function(error) {
@@ -26,18 +23,53 @@ module.exports.acceptInvite = function(req, res, next) {
 module.exports.declineInvite = function(req, res, next) {
   Invite.findById(req.params.inviteID, function(error, invite) {
     if (error) return helper.mongooseValidationError(error, res);
+    if (!invite) return inviteDoesNotExist(res);
 
-    if (!invite) return res.status(404).json({
-      success: false,
-      error: 'Invite does not exist.'
-    })
-
-    invite.remove(function(error) {
-      if (error) return helper.mongooseValidationError(error, res);
-
-      res.status(200).json({
-        success: true
-      })
-    })
+    if (!checkInviteAuth(req.user._id, res, invite.to)) return inviteAuthError(res);
+    removeInvite(invite, res);
   })
 };
+
+module.exports.startWalk = function(req, res, next) {
+  Invite.findById(req.params.inviteID, function(error, invite) {
+    if (error) return helper.mongooseValidationError(error, res);
+    if (!invite) return inviteDoesNotExist(res);
+
+    if (!checkInviteAuth(req.user._id, res, [invite.from])) return inviteAuthError(res);
+    removeInvite(invite, res);
+  });
+};
+
+function inviteDoesNotExist(res) {
+  return res.status(404).json({
+           success: false,
+           error: 'Invite does not exist.'
+         })
+}
+
+function inviteAuthError(res) {
+  return res.status(401).json({
+                  success: false,
+                  error: 'You are not authorised to modify this invite.'
+                })
+}
+
+function checkInviteAuth(userID, res, ids) {
+  var valid = false;
+
+  for (let key in ids) {
+    if (userID.equals(ids[key])) valid = true;
+  }
+
+  return valid;
+}
+
+function removeInvite(invite, res) {
+  invite.remove(function(error) {
+    if (error) return helper.mongooseValidationError(error, res);
+
+    return res.status(200).json({
+             success: true
+           })
+  });
+}
